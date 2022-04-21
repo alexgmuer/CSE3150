db884f3c85fe1f9d00003e5dd8dde40ec9e146b1
alexgmuer <alexander.gmuer@uconn.edu>
2022-04-21 13:19:38 -0400

    all lines in vector then parsing implementation

--- Asgn7/Simulator.hpp
+++ Asgn7/Simulator.hpp
@@ -1,6 +1,10 @@
 #include <fstream>
 #include <iostream>
+#include <stdio.h>
+#include <sstream>
 #include <string>
+#include <map>
+#include <vector>
 // Define data structure inside each node as a class
 // Data structure needs to contain the type 
 
@@ -49,16 +53,73 @@ class Simulator {
             std::fstream filestream;
             filestream.open(filename);
 
+            std::string line;
+            
+            // Need to parse string based around '<-' and '=' characters
+            std::vector<std::string> file_lines;
+
+            // Declare delimiters
+            std::string equal_delim = "=";
+            std::string arrow_delim = "->";
+            
             // Check if file is open
             if (filestream.is_open()) {
-                
-                std::string line;
-
                 // Read data in from file and put into string 
                 while (std::getline(filestream, line)) {
-                    std::cout << line << std::endl;
+                    file_lines.push_back(line);
                 }
+            }
+            
+            // Iterate through file_lines vector and parse each file line
+            for (int i=0;i<file_lines.size();i++) {
+                //std::cout << file_lines[i] << std::endl;
+                
+                // Check if there is a '=' substring or a '->' substring in the line
+                size_t equal_found = file_lines[i].find(equal_delim);
+                size_t arrow_found = file_lines[i].find(arrow_delim);
+                
+                std::map<std::string, std::string> node_lines;
+                std::map<std::string, std::string> connection_lines;
+                // If equal substring is in line
+                if (equal_found != std::string::npos) {
+                    // Temp buffer to hold tokenized parts of line
+                    std::vector<std::string> temp_buf;
 
+                    // Make line into stream
+                    std::istringstream line_stream(file_lines[i]);
+
+                    // temp string 
+                    std::string temp_str;
+
+
+                    while (std::getline(line_stream, temp_str, equal_delim)) {
+                        temp_buf.push_back(temp_str);
+                    }
+
+                    node_lines[temp_buf[0]] = temp_buf[1];
+                }
+                
+                // If arrow substring is in line, add to connection_lines map
+                // Key in each map element is the source, value is destination
+                if (arrow_found != std::string::npos) {
+                    // Temp buffer to hold tokenized parts of line
+                    std::vector<std::string> temp_buf;
+
+                    // Make line into stream
+                    std::istringstream line_stream(file_lines[i]);
+
+                    // temp string 
+                    std::string temp_str;
+
+
+                    while (std::getline(line_stream, temp_str, equal_delim)) {
+                        temp_buf.push_back(temp_str);
+                    }
+
+                    connection_lines[temp_buf[0]] = temp_buf[1];
+                }
+                
+                
             }
 
         }
