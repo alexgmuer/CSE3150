f4ce5001a407c6289d1fd0e40e094483e8565b6e
alexgmuer <alexander.gmuer@uconn.edu>
2022-05-01 19:15:06 -0400

    Test program init

---
+++ Final_prep/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main() {
+
+    std::cout << "\e[32m---ENTERING MAIN---\e[0m" << std::endl;
+
+
+
+    
+    
+    std::cout << "\e[32m---EXITING MAIN---\e[0m" << std::endl;
+    return 0;
+}
---
+++ Final_prep/wiki.hpp
@@ -0,0 +1,7 @@
+// Write in a class in order to read in from the personal_notes.md file,
+// partition the sections into a class hierarchy, overload the >> operator
+// so that the class object can be easily printed as a tree structure, 
+// make it so that the definitions/section bodies can be extracted from the 
+// class object using methods.
+
+// Utilize virtual methods somehow
